90fee9a747e9fdd721c20be1ccf565fc1efc2e79
Anisha Saxena <anisaxena53@gmail.com>
2021-07-30 16:14:30 +0530

    Redux Intro

---
+++ src/redux/configureStore.js
@@ -0,0 +1,9 @@
+import {createStore} from 'redux';
+import {intialState,Reducer} from './reducer';
+export const ConfigureStore=()=>{
+    const store=createStore(
+        Reducer,
+        intialState
+    );
+    return store;
+}
