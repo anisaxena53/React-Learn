f515656cb46a43fa26e5fdaf51009b3116f18a49
Anisha Saxena <anisaxena53@gmail.com>
2021-07-30 16:51:15 +0530

    react-redux-form

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,9 @@
 import {createStore} from 'redux';
-import {intialState,Reducer} from './reducer';
+import {initialState,Reducer} from './reducer';
 export const ConfigureStore=()=>{
     const store=createStore(
         Reducer,
-        intialState
+        initialState
     );
     return store;
 }
