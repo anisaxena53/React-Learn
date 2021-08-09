9b0b64c463c30c16e4e1422692c31c651e4fc286
Anisha Saxena <anisaxena53@gmail.com>
2021-08-09 17:35:53 +0530

    Assignment 4

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
-
+import { baseUrl } from '../shared/baseUrl';
+import { Fade, Stagger } from 'react-animation-components';
 function About(props) {
      
-    const leaders = props.leaders.map((leader) => {
+    const leaders = props.leaders.leaders.map((leader) => {
         return (
             <div key={leader.id} className="col-12 mt-5">
+                <Fade in>
               <Media tag="li">
                 <Media left middle>
-                    <Media object src={leader.image} alt={leader.name} />
+                    <Media object src={baseUrl+leader.image} alt={leader.name} />
                 </Media>
                 <Media body className="ml-5">
                   <Media heading>{leader.name}</Media>
@@ -17,8 +19,10 @@ function About(props) {
                   <p>{leader.description}</p>
                 </Media>
               </Media>
+              </Fade>
             </div>
           );
+          
            
             
     });
@@ -79,7 +83,9 @@ function About(props) {
                 </div>
                 <div className="col-12">
                     <Media list>
+                        <Stagger in>
                         {leaders}
+                        </Stagger>
                     </Media>
                 </div>
             </div>
--- src/redux/forms.js
+++ src/redux/forms.js
@@ -1,9 +1,14 @@
-export const InitialFeedback = {
-    firstname: '',
-    lastname: '',
-    telnum: '',
-    email: '',
-    agree: false,
-    contactType: 'Tel.',
-    message: ''
+import * as ActionTypes from './ActionTypes';
+
+export const InitialFeedback = (state = { errMess: null, feedback:[]}, action) => {
+  switch (action.type) {
+    
+    case ActionTypes.ADD_FEEDBACK:
+        var feedback = action.payload;
+        
+        return { ...state, feedback: state.feedback.concat(feedback)};
+
+    default:
+      return state;
+  }
 };
